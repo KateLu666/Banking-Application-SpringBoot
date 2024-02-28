45706c0fc20a49ab66a1d72385422c9551d23b4a
KateLu666 <KateLuDev@outlook.com>
2024-02-28 17:30:18 -0500

    Implemented check balance when user login

--- src/main/java/com/bankApp/bankApp/Controller/AccountController.java
+++ src/main/java/com/bankApp/bankApp/Controller/AccountController.java
@@ -1,4 +1,36 @@
 package com.bankApp.bankApp.Controller;
 
+import com.bankApp.bankApp.Model.User;
+import com.bankApp.bankApp.Service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpSession;
+
+@RestController
+@RequestMapping(path = "/account")
+@CrossOrigin(origins = {"http://localhost:8080"}, allowCredentials = "true", allowedHeaders = "*", exposedHeaders = "*", maxAge = 360000)
 public class AccountController {
+
+    private final AccountService accountService;
+
+    @Autowired
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @GetMapping("/balance")
+    public ResponseEntity<?> checkBalance(HttpSession session) {
+        User user = (User) session.getAttribute("user");
+        if (user == null) {
+            return ResponseEntity.badRequest().body("No user logged in");
+        }
+        double balance = accountService.checkBalance(user.getUserId());
+        return ResponseEntity.ok(balance);
+    }
+
 }
--- src/main/java/com/bankApp/bankApp/Service/AccountService.java
+++ src/main/java/com/bankApp/bankApp/Service/AccountService.java
@@ -1,4 +1,24 @@
 package com.bankApp.bankApp.Service;
 
+
+import com.bankApp.bankApp.Model.Account;
+import com.bankApp.bankApp.Repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.NoSuchElementException;
+
+@Service
 public class AccountService {
+    private final AccountRepository accountRepository;
+
+    @Autowired
+    public AccountService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    public double checkBalance(int userId) {
+        Account account = accountRepository.findByUser_UserId(userId)
+                .orElseThrow(() -> new NoSuchElementException("Account not found for User ID: " + userId));
+        return account.getBalance();
+    }
 }
